ca4fe50e44164e7dc1cb3a5862cf1ef99c520606
Mati <mateuszbak3@gmail.com>
2016-07-28 17:50:19 +0200

    Sort project list

--- app/src/main/java/bak/mateusz/sourcemeter/ProjectListActivity.java
+++ app/src/main/java/bak/mateusz/sourcemeter/ProjectListActivity.java
@@ -19,6 +19,8 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import bak.mateusz.sourcemeter.model.Project;
@@ -176,6 +178,12 @@ public class ProjectListActivity extends AppCompatActivity {
     @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
     public void onProjectsListEvent(List<Project> event){
         this.projectsList = event;
+        Collections.sort(projectsList, new Comparator<Project>() {
+            @Override
+            public int compare(Project project, Project t1) {
+                return project.getProjectName().compareTo(t1.getProjectName());
+            }
+        });
         snackbar.dismiss();
         setupRecyclerView((RecyclerView) recyclerView);
     }
